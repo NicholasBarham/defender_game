c58486da58b2a76e7f1ef9d712a445ec229ea2f2
Nicholas Barham <nickkeysgaming@gmail.com>
2020-08-26 23:56:17 +0100

    Spaceship movement created

---
+++ Assets/Scripts/Movement/Player/MovementInput.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Util.Variables;
+
+namespace Defender
+{
+    public class MovementInput : MonoBehaviour
+    {
+        [SerializeField] private Vector2Variable inputDirection = null;
+
+        void Update()
+        {
+            UpdateInput();
+        }
+
+        private void UpdateInput()
+        {
+            if(inputDirection.Equals(null)) 
+                return;
+
+            var input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            inputDirection.Value = input;
+        }
+
+        private void OnDisable() 
+        {
+            inputDirection.Value = Vector2.zero;
+        }
+    }
+
+}
