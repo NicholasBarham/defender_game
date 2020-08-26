5e1dc3a74af4940ba6dc58b07cf98def326bd543
Nicholas Barham <nickkeysgaming@gmail.com>
2020-08-26 20:38:25 +0100

    Framework import

---
+++ Assets/Scripts/Utils/Game Event/Presets/Events/Vector3Event.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace Util.GameEvents
+{
+    [CreateAssetMenu(fileName = "Vector3GameEvent", menuName = "Game Events/Vector3")]
+    public class Vector3Event : BaseGameEvent<Vector3> { }
+}
---
+++ Assets/Scripts/Utils/Game Event/Presets/Listeners/Vector3Listener.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+namespace Util.GameEvents
+{
+    public class Vector3Listener : BaseGameEventListener<Vector3, Vector3Event, Vector3UnityEvent> { }
+}
